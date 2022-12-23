177c5206440244cebcb1636a6eb357c8caa29fa2
zhdima <zhdima@gmail.com>
2022-12-23 19:32:05 +0200

    completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+function doTask02() {
+
+  const KEY_PLAYER_TIME = 'videoplayer-current-time';
+  const iframe = document.querySelector('iframe');
+ 
+  if (!iframe) {
+    console.log('Error: invalid markup!');
+    return;
+  }
+
+  const player = new Player(iframe);
+
+  const jsonSavedTime = localStorage.getItem(KEY_PLAYER_TIME);
+  if (jsonSavedTime) {
+    try {
+      player.setCurrentTime(JSON.parse(jsonSavedTime));
+    } catch (err) {
+      console.error('Error: invalid saved time in LocalStorage.' + KEY_PLAYER_TIME);
+      console.error(err);
+    }
+  }
+
+  player.on('timeupdate', throttle(currentTime => 
+    localStorage.setItem(KEY_PLAYER_TIME, JSON.stringify(currentTime.seconds)), 1000));
+}
+
+doTask02();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const KEY_FORM_STATE = 'feedback-form-state';
+
+const getFormState = form => ({ email: form.elements.email.value, message: form.elements.message.value });
+
+function onFormSubmit(evt) {
+  
+  evt.preventDefault();
+
+  console.log(getFormState(evt.currentTarget));
+
+  localStorage.removeItem(KEY_FORM_STATE);
+  evt.currentTarget.reset();
+}
+
+function doTask03() {
+
+  const form = document.querySelector('.feedback-form');
+
+  if (!form) {
+    console.log('Error: invalid markup!');
+    return;
+  }
+
+  const jsonFormState = localStorage.getItem(KEY_FORM_STATE);
+  if (jsonFormState) {
+    try {
+      const formState = JSON.parse(jsonFormState);
+      form.elements.email.value   = formState.email;
+      form.elements.message.value = formState.message;
+    } catch (err) {
+      console.error('Error: invalid saved form state in LocalStorage.' + KEY_FORM_STATE);
+      console.error(err);
+    }
+  }
+
+  form.addEventListener('input', throttle(evt =>
+    localStorage.setItem(KEY_FORM_STATE, JSON.stringify(getFormState(form))), 500));
+  
+  form.addEventListener('submit', onFormSubmit);
+}
+
+doTask03();
