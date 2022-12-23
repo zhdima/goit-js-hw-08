c8b1045e0025e99d3b7f282b5de59ac735591ec1
zhdima <zhdima@gmail.com>
2022-12-23 13:01:06 +0200

    task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,42 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) =>
+    `<li>
+      <a class="gallery__item" href="${original}">
+        <img 
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`).join('');  
+}
+
+function doTask01() {
+
+  const gallery = document.querySelector('.gallery');
+ 
+  if (!gallery) {
+    console.log('Error: invalid markup!');
+    return;
+  }
+
+  gallery.innerHTML = createGalleryMarkup(galleryItems); 
+
+  const opts = {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  };
+
+  const lightbox = new SimpleLightbox('.gallery .gallery__item', opts);
+}
+
+doTask01();
